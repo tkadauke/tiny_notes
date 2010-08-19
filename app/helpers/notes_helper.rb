44ae95122ae375e15bb7149b072cd520431079e6
Thomas Kadauke <tkadauke@imedo.de>
2010-08-20 00:00:06 +0200

    support nested lists in notes

--- app/helpers/notes_helper.rb
+++ app/helpers/notes_helper.rb
@@ -1,13 +1,13 @@
 module NotesHelper
   def render_note(text)
     html = RedCloth.new(text).to_html
-    html.gsub(/<li(.*?)>(.*?)<\/li>/) do
+    html.gsub(/<li(.*?)>(.*?)(<ul>|<\/li>)/m) do
       id = Digest::MD5.hexdigest($2)
       value = ! @statuses.find { |status| status.item_id == id }.nil?
       check_box = check_box_tag "note_item_ids[]", id, value, :id => "item_#{id}", :onchange => "if (this.checked) { $(this).up('li').className='done'; } else { $(this).up('li').className=''; }"
       label = label_tag "item_#{id}", $2
       
-      "<li#{$1}#{' class="done"' if value}>#{check_box} #{label}</li>"
+      "<li#{$1}#{' class="done"' if value}>#{check_box} #{label}#{$3}"
     end
   end
 end
